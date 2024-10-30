10aeeb035830f95e5cd31b8c6308b3ca866db25f
JOAO GABRIEL MENDES <joao.mendes3@alunos.sc.senac.br>
2024-10-30 19:58:05 -0300

    Parte de multiplicação adicionado ao codigo

--- SuperCalculadora/Program.cs
+++ SuperCalculadora/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o primeiro número:");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número:");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = Multiplicar(numero1, numero2);
+        Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+    }
+
+    static double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+}
